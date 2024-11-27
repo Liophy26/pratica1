661a561663ca5198316d24bfa46840f6b6e0893d
Liophy26 <joao.pedro.o.braga@gmail.com>
2024-11-27 17:19:38 -0300

    FINALMENTE KRL!!!!

--- projeto/versao_original.c
+++ projeto/versao_original.c
@@ -13,22 +13,11 @@ struct aluno_t{
 
 int check=0;
 int ano[15];
-int menu = 3;
 int semestre[15];
 int curso[15];
 int escolhacurso;
 float media[15];
 struct aluno_t alunos[15];
-
-    do{
-        if(menu==3){
-    printf("0 - Lansar nota do aluno; \n");
-    printf("1 - Sobre o Programa;\n");
-    printf("11 - Sair;\n");
-    printf("Escolha o que deseja: ");
-    scanf("%i", &menu);
-    if(menu==0){
-        
 for(int i=0; i<15; i++){
 
 while(check==0){
@@ -36,8 +25,7 @@ printf("Insira os dois ultimos numeros do ano de matricula: ");
 scanf("%i", &ano[i]);
 while(getchar()!='\n');
 
-if(0<=ano[i
-]<=24){
+if(0<=ano[i]<=24){
 check=1;
     
         }else{
@@ -64,7 +52,7 @@ printf("2 - Engenharia da computacao\n");
 printf("3 - Gastronomia\n");
 printf("4 - Moda\n");
 printf("5 - Analise e Desenvolvimento de Sistemas\n");
-printf("6 - Cinema\n");
+printf("6 - Cinema");
 scanf("%i", &escolhacurso);
 while(getchar()!='\n');
 
@@ -92,76 +80,40 @@ check=4;
 }else {
     printf("Codigo Invalido. Tente novamente");
 }
-while(check==4){
+
 
 printf("Insira o nome do aluno: ");
 scanf("%[^\n]s", alunos[i].nome );
 while(getchar()!='\n');
-check = 5;
-}
-check = 5;
 
-while(check==5){
+
+
+while(check==4){
 printf("Insira o nota do primeiro bimestre do aluno: ");
 scanf("%f", &alunos[i].nota1 ); 
-alunos[i].nota1 = alunos[i].nota1*0.4;
-while(getchar()!='\n');
-check=6;
 if(0<=alunos[i].nota1<=10){
-    check=6;
+    check=5;
 
 } else{
     printf("Nota invalida. Tente novamente.\n");
-}
-}
+}}
 
-while(check==6){
+while(check==5){
 printf("Insira o nota do segundo bimestre do aluno: ");
 scanf("%f", &alunos[i].nota2 );
-alunos[i].nota2 = alunos[i].nota2 *0.6;
-while(getchar()!='\n');
-
-check=7;
 if(0<=alunos[i].nota2<=10){
-    check=7;
+    check=6;
 }else{
     printf("Nota invalida. Tente novamente.\n");
 }
-}while(check==7){
+}
 
-
+ 
+}
 for(int i=0; i<15; i++){
-    media[i] = alunos[i].nota1 + alunos[i].nota2;
-    printf("%i%i%i%i || %s || %.1f  %.1f  %.1f\n",ano[i],semestre[i],curso[i],alunos[i].matricula,alunos[i].nome ,alunos[i].nota1,alunos[i].nota2, media[i] );
-    menu=3;
-    while(getchar()!='\n');
-    
+    printf("%i%i%i%i || %s || %f \\ %f \\ %f\n",ano[i],semestre[i],curso[i],alunos[i].matricula, alunos[i].nota1,alunos[i].nota2,alunos[i].nota1*0.4 + alunos[i].nota2*0.6 );
 }
 
-} 
-
-
-}
-
-        
-    }
-    }
-    else if(menu==1){
-         printf("O programa tem a funcao de armazenar e calcular a nota dos alunos, basta selecionar a funcao que deseja\n");
-         printf("3 - Voltar\n");
-         scanf("%i", &menu);
-    }
-    else if(menu==11){
-     printf("Programa encerrado\n");
-    }
-    else{
-    
-        printf("Opcao nao encontrada, tente novamente\n");
-        menu = 3;
-    }
-    check = 0;
-    }
-    while(menu!=11);
 return 0;
 
 }
